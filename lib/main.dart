785b1f3c5eaeba01916d21b663b1048b925a84fd
klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>
2024-01-15 15:37:23 +0100

    Starting setup and repetition time

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Expenses(),
+  ));
 }
 
